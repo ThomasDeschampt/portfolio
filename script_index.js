b6491033e8a48bebe193b1a319242f8366eff87d
Thomas Deschampt <94399277+ThomasDeschampt@users.noreply.github.com>
2022-04-03 19:42:46 +0200

    Update script_index.js

--- script_index.js
+++ script_index.js
@@ -276,8 +276,8 @@ const tick = () =>
     const elapsedTime = clock.getElapsedTime()
     controls.update()
             
-    if(sceneReady)
-    {
+//     if(sceneReady)
+//     {
         for(const point of points)
         {
             const screenPosition = point.position.clone()
@@ -302,7 +302,7 @@ const tick = () =>
                 {
                     point.element.classList.add('visible')
                 }
-            }
+//             }
                 
             const translateX = screenPosition.x * sizes.width * 0.5
             const translateY = - screenPosition.y * sizes.height * 0.5

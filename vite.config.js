958257118b6166d77c7c8253a3f3c297cf4d4f0a
Thomas Deschampt <94399277+ThomasDeschampt@users.noreply.github.com>
2022-04-03 18:46:51 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ module.exports = defineConfig({
           "/src/loaders/GLTFLoader.js",
           "/src/loaders/DRACOLoader.js",
           "gsap",
-          "srcipt_info.js",
+          "script_info.js",
         ],
     }
   }

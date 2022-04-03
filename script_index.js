0b868bed4b359a8dd72d44dd59457ffc1faa1c6e
Thomas Deschampt <94399277+ThomasDeschampt@users.noreply.github.com>
2022-04-03 18:05:47 +0200

    Update script_index.js

--- script_index.js
+++ script_index.js
@@ -1,4 +1,4 @@
-import * as THREE from 'src/static/three.module.js'
+import * as THREE from '/src/static/three.module.js'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js'
 import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js'

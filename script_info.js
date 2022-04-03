d6e64da8971c30528fd6231caa28c828482dedc3
Thomas Deschampt <94399277+ThomasDeschampt@users.noreply.github.com>
2022-04-03 18:36:05 +0200

    Update script_info.js

--- script_info.js
+++ script_info.js
@@ -1,5 +1,4 @@
-import * as THREE from '/node_modules/three/build/three.module.js'
-
+import * as THREE from '/src/static/three.module.js'
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
 

a9f26182f385df8ad0564d0da8c7e5f2c9198b54
Thomas Deschampt <94399277+ThomasDeschampt@users.noreply.github.com>
2022-04-03 16:55:04 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,15 +9,15 @@ module.exports = defineConfig({
         main: resolve(__dirname, 'index.html'),
         info: resolve(__dirname, 'info.html'),
         pro: resolve(__dirname, 'projet.html'),
-      }
-      external: [
-        "three",
-        "../node_modules/three/build/three.module.js",
-        "../node_modules/three/examples/jsm/controls/OrbitControls.js",
-        "../node_modules/three/examples/jsm/loaders/GLTFLoader.js",
-        "../node_modules/three/examples/jsm/loaders/DRACOLoader.js",
-        "../node_modules/gsap",
-      ],
+      },
+        external: [
+          "three",
+          "../node_modules/three/build/three.module.js",
+          "../node_modules/three/examples/jsm/controls/OrbitControls.js",
+          "../node_modules/three/examples/jsm/loaders/GLTFLoader.js",
+          "../node_modules/three/examples/jsm/loaders/DRACOLoader.js",
+          "../node_modules/gsap",
+        ],
     }
   }
 })
